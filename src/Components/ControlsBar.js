f76ca6b47eecb1e8e75ea362cccc4f5843a8027d
Sandi Chakravarty <sandi@fortmasongames.com>
2019-03-22 15:28:42 -0700

    spectrogram ws bug fixes

--- src/Components/ControlsBar.js
+++ src/Components/ControlsBar.js
@@ -9,7 +9,7 @@ class ControlBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            expanded: true,
+            expanded: false,
         }
     }
 
--- src/Components/MusicInformationBar.js
+++ src/Components/MusicInformationBar.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import "../css/MusicInformationBar.css"
 
+const { Dispatcher, DispatchEvents } = window.Project;
+
 class MusicInformationBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             expanded: true,
+            showMIR: false,
+        }
+    }
+
+    componentDidMount() {
+        Dispatcher.on(DispatchEvents.MediaReset, this.reset);
+        Dispatcher.on(DispatchEvents.MediaReady, this.ready);
+    }
+
+    reset = () => {
+        this.setState({ showMIR: false });
+    }
+
+    ready = () => {
+        this.setState({ showMIR: true });
+    }
+
+    loadSpec = () => {
+        const mediaPlayer = window.Project.MediaPlayer.instance;
+        if (mediaPlayer) {
+            mediaPlayer.wavesurfer.initPlugin("spectrogram");
         }
     }
 
@@ -26,7 +49,11 @@ class MusicInformationBar extends Component {
                         <span style={{ marginLeft: 5 + 'px' }}>Music Information</span>
                     </span>
                 </div>
-                <div className={expanded} id="" />
+                <div className={expanded} id="">
+                    <div className="mir-container">
+                        <div id="spectrogram" style={{ display: this.state.showMIR ? "block" : "none" }} />
+                    </div>
+                </div>
             </div>
         )
     }
