2e67a7ecbdf092c8fd1100b95b2c9ccbf9fa2481
Sandi Chakravarty <sandi.newton@gmail.com>
2019-04-03 01:03:20 -0700

    refactor library usage

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,25 +1,21 @@
 /* eslint-disable */
 
-const setStateAsync = (obj, state) => {
+export const setStateAsync = (obj, state) => {
     return new Promise((resolve) => {
         obj.setState(state, resolve)
     });
 }
 
-const readFile = filePath => new Promise((resolve, reject) => {
+export const readFile = filePath => new Promise((resolve, reject) => {
     window.electronFS.readFile(filePath, (err, data) => {
         if (err) reject(err);
         else resolve(data);
     });
 });
 
-const writeFile = (filePath, data) => new Promise((resolve, reject) => {
+export const writeFile = (filePath, data) => new Promise((resolve, reject) => {
     window.electronFS.writeFile(filePath, data, (err) => {
         if (err) reject(err);
         else resolve();
     });
 });
-
-exports.setStateAsync = setStateAsync;
-exports.readFile = readFile;
-exports.writeFile = writeFile;
--- src/preload.js
+++ src/preload.js
@@ -1,22 +1,8 @@
-window.remote = require('electron').remote;
-
-window.electronFS = window.remote.require('fs');
-window.dirname = __dirname;
-window.fetch = fetch;
-window.http = require("http");
-window.os = require("os");
-window.Project = {
-    ImportMedia: require("./lib/libWaveSurfer").ImportMedia,
-    MediaPlayer: require("./lib/libWaveSurfer").MediaPlayer,
-    Dispatcher: require("./lib/libDispatcher").Dispatcher,
-    DispatchEvents: require("./lib/libDispatcher").DispatchEvents,
-}
-
+window.electronFS = require("fs");
+window.mm = require("music-metadata");
 
 const isDev = require('electron-is-dev');
-
 window.isDev = isDev;
-
 window.path = null;
 if (window.os.platform() === 'win32') {
     //eslint-disable-next-line
