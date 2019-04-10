c5a761b2c44c30614287a41650aae4220b9229ac
Sandi Chakravarty <sandi.newton@gmail.com>
2019-04-10 01:28:56 -0700

    add nicer looking spectrum analyzer

--- src/Components/Controls/ControlsBar.frequencygraph.js
+++ src/Components/Controls/ControlsBar.frequencygraph.js
@@ -5,14 +5,26 @@ import '../../css/slider.css'
 
 import { Dispatcher, DispatchEvents } from '../../lib/libDispatcher'
 import { MediaPlayer } from '../../lib/libWaveSurfer'
-
-const { requestAnimationFrame, cancelAnimationFrame } = window;
-
+import { AudioMotionInitialize } from './audioMotion'
 
 class FrequencyGraph extends Component {
     constructor(props) {
         super(props);
         this.state = {
+        }
+        this.audioMotion = null;
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.show !== prevProps.show) {
+            if (this.audioMotion) {
+                if (this.props.show) {
+                    this.audioMotion.start();
+                }
+                else {
+                    this.audioMotion.stop();
+                }
+            }
         }
     }
 
@@ -20,70 +32,28 @@ class FrequencyGraph extends Component {
         Dispatcher.on(DispatchEvents.MediaReset, this.reset);
         Dispatcher.on(DispatchEvents.MediaReady, this.ready);
         this.canvas = document.querySelector('#frequency-canvas');
-        this.canvasCtx = this.canvas.getContext("2d");
 
         this.cWidth = this.canvas.width;
         this.cHeight = this.canvas.height;
-        this.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-        this.canvasCtx.fillRect(0, 0, this.cWidth, this.cHeight);
 
-        this.drawRAF = null;
         this.analyser = null;
-        this.dataArray = null;
         this.bufferLength = 0;
 
         this.ready();
     }
 
-    drawGraph = () => {
-        if (this.analyser && this.dataArray && this.bufferLength > 0) {
-            const WIDTH = this.cWidth;
-            const HEIGHT = this.cHeight;
-
-            this.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-            this.canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-            if (this.props.show) {
-                this.drawRAF = requestAnimationFrame(this.drawGraph);
-            }
-            else {
-                return;
-            }
-
-            this.analyser.getByteFrequencyData(this.dataArray);
-            const barWidth = (WIDTH / this.bufferLength) * 2.5;
-            let barHeight;
-            let x = 0;
-
-            for (let i = 0; i < this.bufferLength; i += 1) {
-                barHeight = this.dataArray[i];
-
-                this.canvasCtx.fillStyle = 'rgb(50,' + (barHeight + 100) + ',50)';
-                this.canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
-
-                x += barWidth + 1;
-            }
-        }
-    }
-
     reset = () => {
-        if (this.drawRAF) {
-            cancelAnimationFrame(this.drawRAF);
-        }
-        this.canvasCtx.clearRect(0, 0, this.cWidth, this.cHeight);
     }
 
     ready = () => {
         const mediaPlayer = MediaPlayer.instance;
         if (mediaPlayer) {
             this.analyser = mediaPlayer.getPostAnalyser();
-            this.analyser.fftSize = 32768;
+            this.analyser.fftSize = 8192;
             this.bufferLength = this.analyser.frequencyBinCount;
-            this.dataArray = new Uint8Array(this.bufferLength);
 
-            this.canvasCtx.clearRect(0, 0, this.cWidth, this.cHeight);
+            this.audioMotion = AudioMotionInitialize(mediaPlayer.audioContext, this.analyser, this.canvas, this.bufferLength);
         }
-        this.drawGraph();
     }
 
     render() {
