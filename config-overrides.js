f8b881751f837234b11e58232fa888ef93e89540
Sandi Chakravarty <sandi.newton@gmail.com>
2019-04-19 03:30:58 -0700

    config overrides update for build failing due to worklets

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,41 @@
 //config-overrides.js
 const rewireEslint = require('react-app-rewire-eslint');
-module.exports = function override(config, env) {
-    config.module.rules.push({
-        test: /worklet\.js$/,
-        loader: require.resolve('worklet-loader'),
-        options: {
-            name: 'js/[hash].worklet.js'
-        }
-    })
 
+const path = require('path');
+
+const getChildrenRules = (loader) => loader.use || loader.oneOf || (Array.isArray(loader.loader) && loader.loader) || [];
+
+const findIndexAndRules = (rulesSource, ruleMatcher) => {
+    let result;
+    const rules = Array.isArray(rulesSource) ? rulesSource : getChildrenRules(rulesSource);
+
+    rules.some((rule, index) => (
+        result = ruleMatcher(rule) ? { index, rules } : findIndexAndRules(getChildrenRules(rule), ruleMatcher))
+    );
+
+    return result;
+};
+
+const addBeforeRule = (rulesSource, ruleMatcher, value) => {
+    const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
+
+    rules.splice(index, 0, value);
+};
+
+const fileLoaderRuleMatcher = (rule) => rule.loader && rule.loader.indexOf(`${path.sep}file-loader${path.sep}`) !== -1;
+module.exports = function override(config, env) {
     config = rewireEslint(config, env);
+    const rule = {
+        test: /worklet\.js$/,
+        use: [
+            {
+                loader: require.resolve('worklet-loader'),
+                options: {
+                    name: 'js/[hash].worklet.js'
+                }
+            }
+        ]
+    }
+    //addBeforeRule(config.module.rules, fileLoaderRuleMatcher, rule);
     return config;
 }
