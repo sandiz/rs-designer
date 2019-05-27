dd797e7c057308b48b28995aff13b0ab38fce826
Sandi Chakravarty <sandi@fortmasongames.com>
2019-05-26 18:56:34 -0700

    hzoom

--- src/Components/Analysis/AnalysisBar.js
+++ src/Components/Analysis/AnalysisBar.js
@@ -7,9 +7,9 @@ import "../../css/AnalysisBar.css"
 import { DispatcherService, DispatchEvents, KeyboardEvents } from '../../services/dispatcher'
 import { MediaPlayer } from '../../lib/libWaveSurfer'
 import ForageService, { SettingsForageKeys } from '../../services/forage.js';
-import { toggleNeverland, setStateAsync } from '../../lib/utils';
 import ProjectService from '../../services/project';
 import { SettingsService } from '../../services/settings';
+import { setStateAsync } from '../../lib/utils';
 
 class AnalysisBar extends Component {
     constructor(props) {
@@ -21,9 +21,16 @@ class AnalysisBar extends Component {
             currentVZoom: 1,
             currentZoom: 1,
         }
+        this.zoom = {
+            max: 40,
+            min: 1,
+            default: 5,
+            increment: 1000, /* increment by 1000 pixels */
+        }
         this.se_excludes = ['showMIR', 'analysing']
         this.containerRef = React.createRef();
         this.specRef = React.createRef();
+        this.raf = 0;
     }
 
     componentWillUnmount = () => {
@@ -54,7 +61,6 @@ class AnalysisBar extends Component {
     aboutoDraw = (type) => {
         if (type !== "cqt") return;
         if (this.state.expanded === false) {
-            toggleNeverland(this.containerRef, true);
             this.containerRef.current.classList.toggle("show", true);
         }
     }
@@ -62,12 +68,12 @@ class AnalysisBar extends Component {
     finishedDrawing = (type) => {
         if (type !== "cqt") return;
         if (this.state.expanded === false) {
-            toggleNeverland(this.containerRef, false);
             this.containerRef.current.classList.toggle("show", false);
         }
     }
 
     analyseStart = (method) => {
+        if (this.raf) cancelAnimationFrame(this.raf);
         this.setState({ analysing: true });
         //this.toastId = toaster('', 'fas fa-hourglass-half', 'Analysing media in the background...', { autoClose: false });
         this.toastId = toast(({ closeToast }) => (
@@ -103,7 +109,7 @@ class AnalysisBar extends Component {
                 autoClose: (800 / window.os.cpus().length) * 1000, /* dumb heuristic, mbp i7-4770HQ CPU @ 2.20GHz * 8 takes ~100 seconds */
                 pauseOnFocusLoss: false,
                 pauseOnHover: false,
-            })
+            });
     }
 
     analyseEnd = async (method) => {
@@ -124,6 +130,11 @@ class AnalysisBar extends Component {
             ),
         });
         this.cqtImageRender();
+        this.playHeadRender();
+    }
+
+    playHeadRender = async () => {
+        this.raf = requestAnimationFrame(this.playHeadRender);
     }
 
     cqtImageRender = async () => {
@@ -132,10 +143,12 @@ class AnalysisBar extends Component {
             const info = ProjectService.getProjectInfo();
             img.src = "file:///" + info.cqt;
             // eslint-disable-next-line
-            img.onload = function () {
+            img.onload = () => {
+                this.cqtDims.w = img.width;
+                this.cqtDims.h = img.height;
                 img.style.display = "";
-                img.style.width = this.width + 'px';
-                img.style.height = 512 + 'px';
+                img.style.width = this.containerRef.current.offsetWidth + 'px';
+                img.style.height = this.containerRef.current.offsetHeight + 'px';
             }
         }
     }
@@ -148,11 +161,35 @@ class AnalysisBar extends Component {
         this.setState({ showMIR: true });
     }
 
-    zoom = async (type) => {
+    zoomfn = async (type) => {
         if (type === "stretch") {
-            if (this.state.currentVZoom === 1) await setStateAsync(this, { currentVZoom: 2 });
-            else await setStateAsync(this, { currentVZoom: 1 });
-            this.specRef.current.style.height = (512 * this.state.currentVZoom) + 'px';
+            let zoom = this.state.currentVZoom;
+            if (zoom === 1) zoom = 2;
+            else zoom = 1;
+            this.setState({ currentVZoom: zoom })
+            this.specRef.current.style.height = (this.containerRef.current.offsetHeight * zoom) + 'px';
+        }
+        else if (type === "inc") {
+            const curr = this.state.currentZoom;
+            if (curr < this.zoom.max) {
+                await setStateAsync(this, { currentZoom: curr + 1 });
+            }
+            const diff = (this.state.currentZoom - curr);
+            const p = parseFloat(this.specRef.current.style.width);
+            this.specRef.current.style.width = (p + (diff * this.zoom.increment)) + 'px';
+        }
+        else if (type === "dec") {
+            const curr = this.state.currentZoom;
+            if (curr > this.zoom.min) {
+                await setStateAsync(this, { currentZoom: curr - 1 });
+            }
+            const diff = (this.state.currentZoom - curr);
+            const p = parseFloat(this.specRef.current.style.width);
+            this.specRef.current.style.width = (p + (diff * this.zoom.increment)) + 'px';
+        }
+        else if (type === "reset") {
+            this.specRef.current.style.width = this.containerRef.current.offsetWidth + 'px';
+            this.specRef.current.style.height = this.containerRef.current.offsetHeight + 'px';
         }
     }
 
@@ -186,7 +223,7 @@ class AnalysisBar extends Component {
         }
     }
 
-    render() {
+    render = () => {
         const expanded = "mir-collapse-root bg-light " + (this.state.expanded ? "collapse show" : "collapse");
         const faclass = this.state.expanded ? "fas fa-caret-down" : "fas fa-caret-right"
         return (
@@ -213,15 +250,15 @@ class AnalysisBar extends Component {
                         </span>
                         <span className="dot-separator"> • </span>
                         <span>
-                            <i className="cur-pointer fas fa-arrows-alt-v" title="Zoom Vertically" onClick={e => this.zoom('stretch')} />
+                            <i className="cur-pointer fas fa-arrows-alt-v" title="Zoom Vertically" onClick={e => this.zoomfn('stretch')} />
                         </span>
                         <span className="dot-separator"> • </span>
-                        <i className="cur-pointer fas fa-search-minus" onClick={e => this.zoom('dec')} />
+                        <i className="cur-pointer fas fa-search-minus" onClick={e => this.zoomfn('dec')} />
                         &nbsp;
                         <span>{this.state.currentZoom}x</span>
                         &nbsp;&nbsp;
-                        <i className="cur-pointer fas fa-search-plus" onClick={e => this.zoom('inc')} />
-                        <i className="cur-pointer fas fa-search" onClick={e => this.zoom('reset')} />
+                        <i className="cur-pointer fas fa-search-plus" onClick={e => this.zoomfn('inc')} />
+                        <i className="cur-pointer fas fa-search" onClick={e => this.zoomfn('reset')} />
                     </span>
                 </div>
                 <div ref={this.containerRef} className={expanded} id="mir-vis-container">
